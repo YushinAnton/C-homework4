4b06e83a9d0fa51ae4d004c224e41d721dc450fe
Anton_Yushin <ya.napishimne@yandex.ru>
2023-04-22 10:11:46 +0300

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число A");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = int.Parse(Console.ReadLine());
+if (b > 0)
+    {
+        double c = Math.Pow(a, b); 
+        Console.WriteLine($"A в степени B равно {c}");
+    }
+    
+   else 
+   {
+    Console.WriteLine("ERROR");
+   }
+
+
+
+
