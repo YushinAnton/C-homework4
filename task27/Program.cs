b0a0d6b3360129e4ec8957ba5c3c1c2f10ebc822
Anton_Yushin <ya.napishimne@yandex.ru>
2023-04-22 10:41:48 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,10 @@
+﻿int Sum(int number)
+{
+    int result = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+Console.WriteLine(Sum(int.Parse(Console.ReadLine())));
