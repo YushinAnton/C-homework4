6049bf96c8f75bacf172ad683bc1ed48f666ec3d
Anton_Yushin <ya.napishimne@yandex.ru>
2023-04-22 11:28:50 +0300

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("введите число ");
+int number = int.Parse(Console.ReadLine());
+int[] Array = new int[number];
+for (int i = 0; i < Array.Length; i++)
+{
+    Array[i] = new Random().Next(1,100);
+    //Console.Write(Array[i] + " ");
+}
+Console.Write(Array[i] + " ");
